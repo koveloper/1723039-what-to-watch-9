96a7e91e771743d49eefbd0417b76b2c2cd63ea4
koveloper <koveloper@gmail.com>
2022-02-18 22:47:52 +0300

    refactor add-review form

--- project/src/components/add-review-form/add-review-form.tsx
+++ project/src/components/add-review-form/add-review-form.tsx
@@ -1,22 +1,10 @@
-const getStarElement = (value: number): JSX.Element => (
-  <>
-    <input className="rating__input" id={`star-${value}`} type="radio" name="rating" value={`${value}`} />
-    <label className="rating__label" htmlFor={`star-${value}`}>Rating {`${value}`}</label>
-  </>
-);
+import RatingChooser from './rating-chooser';
 
 function AddReviewForm(): JSX.Element {
-  const ratingChooser = (
-    <div className="rating">
-      <div className="rating__stars">
-        {new Array(10).fill(null).map((v, i) => getStarElement(10 - i))}
-      </div>
-    </div>
-  );
   return (
     <div className="add-review">
       <form action="#" className="add-review__form">
-        {ratingChooser}
+        <RatingChooser />
         <div className="add-review__text">
           <textarea className="add-review__textarea" name="review-text" id="review-text" placeholder="Review text"></textarea>
           <div className="add-review__submit">
