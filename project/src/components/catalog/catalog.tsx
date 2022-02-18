2e0ec860336b09ce3f31280c42dbb4869acf6945
koveloper <koveloper@gmail.com>
2022-02-18 22:37:44 +0300

    refactor catalog component

--- project/src/components/catalog/catalog.tsx
+++ project/src/components/catalog/catalog.tsx
@@ -1,23 +1,16 @@
 import GenresList from '../genres-list/genres-list';
 import FilmsList from '../films-list/films-list';
+import ShowMoreButton from './show-more-button';
 import { FilmDataProps } from '../../types/film-data-type';
 import { GenreProps } from '../../types/genre-type';
-
-const getGenresListElement = (genres?: GenreProps[]): JSX.Element | null => genres ? <GenresList genres={genres} /> : null;
-
-const getShowMoreButton = (isShown: boolean): JSX.Element | null => isShown ? (
-  <div className="catalog__more">
-    <button className="catalog__button" type="button">Show more</button>
-  </div>
-) : null;
 
 function Catalog({genres, films, showMoreButton}: {genres?: GenreProps[], films: FilmDataProps[], showMoreButton: boolean}): JSX.Element {
   return (
     <section className="catalog">
       <h2 className="catalog__title visually-hidden">Catalog</h2>
-      {getGenresListElement(genres)}
+      {genres ? <GenresList genres={genres} /> : null}
       <FilmsList films={films}/>
-      {getShowMoreButton(showMoreButton)}
+      {showMoreButton ? <ShowMoreButton /> : null}
     </section>
   );
 }
