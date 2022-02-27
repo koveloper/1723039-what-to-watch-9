3194ae74511350bfcddd423d2211bd2075a9e4c8
koveloper <koveloper@gmail.com>
2022-02-27 17:31:31 +0300

    add video playback on film logo after second passed from hover event

--- project/src/components/film-logo/film-logo.tsx
+++ project/src/components/film-logo/film-logo.tsx
@@ -1,31 +1,58 @@
-import { SyntheticEvent } from 'react';
+import { SyntheticEvent, useEffect, useRef, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import { FilmDataType } from '../../types/film-data-type';
 import { AppRoute } from '../../utils/constants';
 
 type FilmLogoProps = {
   film: FilmDataType;
-  onHover: (id: number) => void;
 }
 
-function FilmLogo({film, onHover} : FilmLogoProps): JSX.Element {
+function FilmLogo({film} : FilmLogoProps): JSX.Element {
   const navigate = useNavigate();
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+  const [isHovered, setHovered] = useState(false);
   const onMouseEnterHandler = (evt: SyntheticEvent) => {
     evt.stopPropagation();
-    onHover(film.id);
+    setHovered(true);
   };
   const onMouseExitHandler = (evt: SyntheticEvent) => {
     evt.stopPropagation();
-    onHover(-1);
+    setHovered(false);
   };
+  useEffect(() => {
+    const player = videoRef.current;
+    const playFunc = () => {
+      if(timerId) {
+        clearTimeout(timerId);
+      }
+      if(!player) {
+        return;
+      }
+      player.src = film.videoLink;
+      player.play();
+    };
+    const timerId = isHovered ? setTimeout(playFunc, 1000) : null;
+    return () => {
+      if(timerId) {
+        clearTimeout(timerId);
+      }
+      if(!player) {
+        return;
+      }
+      player.pause();
+      player.src = '';
+    };
+  }, [isHovered, film.videoLink]);
+
   const onClickHandler = () => navigate(`${AppRoute.Films}/${film.id}`);
   return (
     <article onClick={onClickHandler} onMouseLeave={onMouseExitHandler} onMouseEnter={onMouseEnterHandler} className="small-film-card catalog__films-card">
       <div className="small-film-card__image">
-        <img src={film.previewImage} alt={film.name} width="280" height="175" />
+        <video ref={videoRef} poster={film.previewImage} width="280" height="175"/>
       </div>
       <h3 className="small-film-card__title">
-        <a className="small-film-card__link" href={film.videoLink}>{film.name}</a>
+        <Link className="small-film-card__link" to={`${AppRoute.Films}/${film.id}`}>{film.name}</Link>
       </h3>
     </article>
   );
--- project/src/components/films-list/films-list.tsx
+++ project/src/components/films-list/films-list.tsx
@@ -1,17 +1,14 @@
 import FilmLogo from '../film-logo/film-logo';
 import { FilmDataType } from '../../types/film-data-type';
-import { useState } from 'react';
 
 type FilmsListProps = {
   films: FilmDataType[];
 }
 
 function FilmsList({ films }: FilmsListProps): JSX.Element {
-  const [hoverCardId, setHoverCard] = useState(-1);
   return (
     <div className="catalog__films-list">
-      {films.map((film) => <FilmLogo key={`film-preview-${film.id}`} film={film} onHover={(id) => setHoverCard(id)}/>)}
-      <div className='visually-hidden'>{hoverCardId}</div>
+      {films.map((film) => <FilmLogo key={film.id.toString()} film={film}/>)}
     </div>
   );
 }
