dd0dcd9fdd4d2cbe2602898f99a125c17c1fefec
koveloper <koveloper@gmail.com>
2022-03-12 12:55:24 +0300

    move spinner styles to separate css class

--- project/src/components/spinner/spinner.tsx
+++ project/src/components/spinner/spinner.tsx
@@ -1,18 +1,6 @@
 function Spinner(): JSX.Element {
-  const styleObject: React.CSSProperties = {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    textAlign: 'center',
-    width: '100%',
-    height: '100%',
-    minHeight: '100vh',
-    background: 'rgba(30,30,30,0.8)',
-    backdropFilter: 'blur(8px)',
-    color: 'white',
-  };
   return (
-    <div style={styleObject}>Loading...<br/>Please wait...</div>
+    <div className="spinner">Loading...<br/>Please wait...</div>
   );
 }
 
