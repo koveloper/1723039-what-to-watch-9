abc243167c9078db3c41a7729bcaa293b9eefda2
koveloper <koveloper@gmail.com>
2022-03-16 12:52:21 +0300

    add redirect to user page on avatar click

--- project/src/components/user-block/user-info.tsx
+++ project/src/components/user-block/user-info.tsx
@@ -1,10 +1,12 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { AppRoute } from '../../utils/constants';
 import { getBlankLink } from '../../utils/logic-utils';
 
 function UserInfo():JSX.Element {
+  const navigate = useNavigate();
   return (
     <>
-      <li className="user-block__item">
+      <li onClick={() => navigate(AppRoute.User)} className="user-block__item">
         <div className="user-block__avatar">
           <img src="img/avatar.jpg" alt="User avatar" width="63" height="63" />
         </div>
