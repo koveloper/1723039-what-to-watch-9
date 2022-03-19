44d5c48e8fe05980721c2e2413d592de1053c669
koveloper <koveloper@gmail.com>
2022-03-19 18:24:54 +0300

    create filmsProcess tests

---
+++ project/src/store/films-process/films-process.test.ts
@@ -0,0 +1,42 @@
+import { createFakeFilmData, createFakeFilmFullData, createFakeFilms } from '../../utils/mocks';
+import { FilmsState } from '../types';
+import { filmsProcess, setFilms, setFullDataFilm, setPromoFilm } from './films-process';
+
+describe('Reducer: filmsProcess', () => {
+  const initialState:FilmsState = {
+    films: null,
+    promoFilm: null,
+    fullDataFilms: {},
+  };
+  it('check for initial state on any non data call', () => {
+    expect(filmsProcess.reducer(void 0, {type: 'some dummy action'}))
+      .toEqual(initialState);
+  });
+  describe('films tree', () => {
+    const fakeFilms = createFakeFilms();
+    it('on setFilms action', () => {
+      expect(filmsProcess.reducer(initialState, setFilms(fakeFilms)))
+        .toEqual(Object.assign({}, initialState, {films: fakeFilms}));
+    });
+  });
+  describe('promoFilm tree', () => {
+    const fakeFilm = createFakeFilmData();
+    it('on setPromoFilm action', () => {
+      expect(filmsProcess.reducer(initialState, setPromoFilm(fakeFilm)))
+        .toEqual(Object.assign({}, initialState, {promoFilm: fakeFilm}));
+    });
+  });
+  describe('fullDataFilms tree', () => {
+    const fakeFilm = createFakeFilmFullData();
+    const state = Object.assign({}, initialState);
+    it('on setPromoFilm action', () => {
+      expect(filmsProcess.reducer(state, setFullDataFilm(fakeFilm)))
+        .toEqual(Object.assign(state, {fullDataFilms: {[fakeFilm.id]: fakeFilm}}));
+    });
+    const anotherFakeFilm = createFakeFilmFullData();
+    it('on another setPromoFilm action', () => {
+      expect(filmsProcess.reducer(state, setFullDataFilm(anotherFakeFilm)))
+        .toEqual(Object.assign(state, {fullDataFilms: Object.assign(state.fullDataFilms, {[anotherFakeFilm.id]: anotherFakeFilm})}));
+    });
+  });
+});
--- project/src/utils/mocks.ts
+++ project/src/utils/mocks.ts
@@ -1,11 +1,15 @@
 import fakerStatic from 'faker';
+import { Comment } from '../types/commentary';
+import { FilmData, FilmFullData, Films } from '../types/film-data-type';
 import { UserWithoutTokenType } from '../types/user-type';
+
+const createRandomName = () => `${fakerStatic.name.firstName()} $${fakerStatic.name.lastName()}`;
 
 export const createFakeUser = (): UserWithoutTokenType => ({
   avatarUrl: fakerStatic.internet.url(),
   id: fakerStatic.datatype.number(),
   email: fakerStatic.internet.email(),
-  name: `${fakerStatic.name.firstName()} ${fakerStatic.name.lastName()}`,
+  name: createRandomName(),
 });
 
 export const createFakeFavoriteFilmsIdList = (): number[] => fakerStatic.datatype.array(fakerStatic.datatype.number(8)).map(
@@ -16,3 +20,46 @@ export const createFakeFavoriteFilmsIdList = (): number[] => fakerStatic.datatyp
   }
   return acc;
 }, [] as number[]);
+
+export const createFakeFilmData = (idValue?: number): FilmData => ({
+  name: fakerStatic.name.title(),
+  posterImage: fakerStatic.image.imageUrl(),
+  previewImage: fakerStatic.image.imageUrl(),
+  backgroundImage: fakerStatic.internet.url(),
+  backgroundColor: fakerStatic.commerce.color(),
+  description: fakerStatic.lorem.paragraphs(1 + fakerStatic.datatype.number(2)),
+  rating: 1 + fakerStatic.datatype.number(10),
+  scoresCount: fakerStatic.datatype.number(30000),
+  director: createRandomName(),
+  starring: fakerStatic.datatype.array(1 + fakerStatic.datatype.number(5)).map((el) => createRandomName()),
+  runTime: 50 + fakerStatic.datatype.number(90),
+  genre: fakerStatic.name.title(),
+  released: 2000 + fakerStatic.datatype.number(21),
+  id: idValue ? idValue : fakerStatic.datatype.number(20),
+  videoLink: fakerStatic.internet.url(),
+  previewVideoLink: fakerStatic.internet.url(),
+  isFavorite: fakerStatic.datatype.boolean(),
+});
+
+export const createFakeComment = (): Comment => ({
+  id: fakerStatic.datatype.number(1000),
+  rating: fakerStatic.datatype.number(10),
+  comment: fakerStatic.lorem.paragraph(),
+  date: fakerStatic.date.past().toISOString(),
+  user: {
+    id: fakerStatic.datatype.number(1000),
+    name: createRandomName(),
+  },
+});
+
+export const createFakeFilmFullData = (): FilmFullData => (() => {
+  const id = fakerStatic.datatype.number(20);
+  return {
+    id,
+    data: createFakeFilmData(id),
+    similar: createFakeFilms(fakerStatic.datatype.number(8)),
+    comments: Array(fakerStatic.datatype.number(8)).fill(null).map(() => createFakeComment()),
+  };
+})();
+
+export const createFakeFilms = (count?: number): Films => Array(count ? count : 30).fill(null).map((el) => createFakeFilmData());
