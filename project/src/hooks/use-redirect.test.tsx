d17e3aa35abfa36df46ff0e6a50e43f54ad29f94
koveloper <koveloper@gmail.com>
2022-03-26 20:13:37 +0300

    add useRedirect hook and cover it with tests

---
+++ project/src/hooks/use-redirect.test.tsx
@@ -0,0 +1,58 @@
+import { configureMockStore } from '@jedmao/redux-mock-store';
+import { Action } from '@reduxjs/toolkit';
+import { renderHook } from '@testing-library/react-hooks';
+import { Provider } from 'react-redux';
+import { AuthStatus } from '../store/constants';
+import { State } from '../store/types';
+import { BrowserRouter } from 'react-router-dom';
+import { setRedirect } from '../store/service-process/service-process';
+import { useRedirect } from './use-redirect';
+
+describe('Hook: useRedirect', () => {
+  const mockStore = configureMockStore<State, Action>();
+  const initialState:State = {
+    user: {
+      authStatus: AuthStatus.Unknown,
+      userData: null,
+      favoriteFilmsIdList: null,
+    },
+    films: {
+      all: null,
+      promoFilm: null,
+      fullDataFilms: {},
+    },
+    service: {
+      redirect: null,
+    },
+  };
+
+  it('should return function and add setRedirect action to store', async () => {
+    const store = mockStore(Object.assign(initialState, {user: {authStatus: AuthStatus.Unknown}}));
+    const fakeRedirectTo = '/fake/redirect/to';
+    const {result} = renderHook(
+      () => useRedirect(),
+      {
+        wrapper: ({ children }) => (
+          <Provider store={store} >
+            <BrowserRouter>
+              {children}
+            </BrowserRouter>
+          </Provider>
+        ),
+      },
+    );
+    //check hook returns function
+    const redirect = result.current;
+    expect(redirect).toBeInstanceOf(Function);
+    //check - there is no actions in store
+    expect(store.getActions()).toEqual([]);
+    //call hook
+    redirect(fakeRedirectTo);
+    //check actions in queue after hook invoke
+    expect(store.getActions().length).toBe(1);
+    const redirectAction = store.getActions().find(({type}) => type === setRedirect.toString());
+    expect(redirectAction).not.toBe(undefined);
+    //check action payload
+    expect(redirectAction && ('payload' in redirectAction) && redirectAction['payload']).toBe(fakeRedirectTo);
+  });
+});
