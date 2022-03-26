024091816dd328af94b625b223e15e8a8a88418a
koveloper <koveloper@gmail.com>
2022-03-26 21:04:06 +0300

    refactor SignIn page on useRedirect custom hook

--- project/src/pages/sign-in-page/sign-in-page.tsx
+++ project/src/pages/sign-in-page/sign-in-page.tsx
@@ -1,13 +1,12 @@
 import SignInForm from '../../components/sign-in-form/sign-in-form';
 import Footer from '../../components/footer/footer';
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import Header from '../../components/header/header';
 import { api } from '../../api/api';
 import { LoginData } from '../../types/login-data';
 import { AppRoute } from '../../utils/constants';
-import Header from '../../components/header/header';
 import { HeaderType } from '../../components/header/header-type';
 import { useAuth } from '../../hooks/use-auth';
+import { useRedirect } from '../../hooks/use-redirect';
 
 type SignInPageProps = {
   message?: string;
@@ -16,15 +15,13 @@ type SignInPageProps = {
 
 export default function SignInPage({message, isError}: SignInPageProps): JSX.Element {
   const isAuthorized = useAuth();
-  const navigate = useNavigate();
+  const redirect = useRedirect();
   const onSubmitHandler = (props: LoginData) => {
     api.login(props);
   };
-  useEffect(() => {
-    if(isAuthorized) {
-      navigate(AppRoute.Root);
-    }
-  }, [isAuthorized]);
+  if(isAuthorized) {
+    redirect(AppRoute.Root);
+  }
   return (
     <div className="user-page">
       <Header type={HeaderType.UserOrSignIn}>
