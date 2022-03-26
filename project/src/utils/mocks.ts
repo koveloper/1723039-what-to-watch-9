cc50ff6c43069cce78aefd93ddf9fd22199a1f82
koveloper <koveloper@gmail.com>
2022-03-26 22:34:26 +0300

    make mock correction

--- project/src/utils/mocks.ts
+++ project/src/utils/mocks.ts
@@ -27,7 +27,7 @@ export const createFakeFilmData = (idValue?: number): FilmData => ({
   name: generator.name.title(),
   posterImage: generator.image.imageUrl(),
   previewImage: generator.image.imageUrl(),
-  backgroundImage: generator.internet.url(),
+  backgroundImage: generator.image.imageUrl(),
   backgroundColor: generator.commerce.color(),
   description: generator.lorem.paragraphs(1 + generator.datatype.number(2)),
   rating: 1 + generator.datatype.number(10),
