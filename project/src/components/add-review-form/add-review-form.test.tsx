8ee0c60eac21b194cbf3a12ac6c7b8b261396f43
koveloper <koveloper@gmail.com>
2022-03-27 20:28:14 +0300

    fix AddReviewForm for check review text length and user rating input

--- project/src/components/add-review-form/add-review-form.test.tsx
+++ project/src/components/add-review-form/add-review-form.test.tsx
@@ -1,7 +1,8 @@
 import userEvent from '@testing-library/user-event';
 import AddReviewForm from './add-review-form';
-import {render, screen} from '@testing-library/react';
+import { render, screen} from '@testing-library/react';
 import { createFakeParagraph } from '../../utils/mocks';
+import { MAX_REVIEW_LENGTH, MIN_REVIEW_LENGTH } from '../../utils/constants';
 
 describe('Component: AddReviewForm', () => {
   it('should render correctly', () => {
@@ -14,8 +15,57 @@ describe('Component: AddReviewForm', () => {
     expect(screen.getAllByRole('radio').length).toBe(10);
   });
 
+  it('should set post button to disabled state in case of review text size outside bounds [50, 400]', () => {
+    render(
+      <AddReviewForm onReviewSubmit={(rating: number, commentary: string) => {
+        const snapshot = screen.getByText('---');
+        if(snapshot) {
+          snapshot.textContent = `${commentary} with mark ${rating}`;
+        }
+      }}
+      />,
+    );
+    //simulate user rating choise
+    userEvent.click(screen.getByText('Rating 3'));
+
+    //outside threshold: < min
+    userEvent.type(screen.getByRole('textbox'), new Array(MIN_REVIEW_LENGTH - 1).fill('a').join(''));
+    //check that submit button disabled for text size less than min threshold
+    expect((screen.getByRole('button') as HTMLButtonElement).disabled).toBe(true);
+
+    //inside thresholds: min
+    userEvent.type(screen.getByRole('textbox'), 'a');
+    expect((screen.getByRole('button') as HTMLButtonElement).disabled).toBe(false);
+    //inside thresholds: max
+    userEvent.type(screen.getByRole('textbox'), new Array((MAX_REVIEW_LENGTH - MIN_REVIEW_LENGTH)).fill('a').join(''));
+    expect((screen.getByRole('button') as HTMLButtonElement).disabled).toBe(false);
+
+    //outside threshold: > max
+    userEvent.type(screen.getByRole('textbox'), 'a');
+    //check that submit button disabled for text size greater than max threshold
+    expect((screen.getByRole('button') as HTMLButtonElement).disabled).toBe(true);
+  });
+
+  it('should set post button to disabled state in case of NO user rating choise DONE', () => {
+    render(
+      <AddReviewForm onReviewSubmit={(rating: number, commentary: string) => {
+        const snapshot = screen.getByText('---');
+        if(snapshot) {
+          snapshot.textContent = `${commentary} with mark ${rating}`;
+        }
+      }}
+      />,
+    );
+    //simulate input of text with size in threshold
+    userEvent.type(screen.getByRole('textbox'), new Array(MIN_REVIEW_LENGTH + 1).fill('a').join(''));
+    //check that submit button disabled without user rating choise
+    expect((screen.getByRole('button') as HTMLButtonElement).disabled).toBe(true);
+  });
   it('should fire onReviewSubmit with review text on film rating', () => {
-    const fakeReview = createFakeParagraph();
+    let fakeReview = createFakeParagraph();
+    while(fakeReview.length < MIN_REVIEW_LENGTH) {
+      fakeReview += '.';
+    }
     const fakeRatingNum = 3;
     render(
       <>
@@ -32,8 +82,7 @@ describe('Component: AddReviewForm', () => {
     //test for redirect to fake page on click
     expect(screen.getByText(/---/i)).toBeInTheDocument();
     //change review text and simulate rating chooser click
-    const textarea = screen.getByRole('textbox') as HTMLTextAreaElement;
-    textarea.value = fakeReview;
+    userEvent.type(screen.getByRole('textbox'), fakeReview);
     userEvent.click(screen.getByText(`Rating ${fakeRatingNum.toString()}`));
     //fire add review click and check it's values
     userEvent.click(screen.getByRole('button'));
--- project/src/utils/constants.ts
+++ project/src/utils/constants.ts
@@ -30,14 +30,17 @@ export enum FilmMark {
     VeryGood = 'Very good',
     Awesome = 'Awesome'
 }
-export const APP_TITLE = 'WTW';
-export const ALL_GENRES = 'All genres';
-export const FILMS_ON_PAGE_INITIAL = 8;
-export const FILMS_ON_PAGE_STEP = 8;
-export const MAX_GENRES_ON_SCREEN = 10;
-
 export enum NameSpace {
     Films = 'films',
     User = 'user',
     Service = 'service',
 }
+
+export const APP_TITLE = 'WTW';
+export const ALL_GENRES = 'All genres';
+export const FILMS_ON_PAGE_INITIAL = 8;
+export const FILMS_ON_PAGE_STEP = 8;
+export const MAX_GENRES_ON_SCREEN = 10;
+export const MIN_REVIEW_LENGTH = 50;
+export const MAX_REVIEW_LENGTH = 400;
+
