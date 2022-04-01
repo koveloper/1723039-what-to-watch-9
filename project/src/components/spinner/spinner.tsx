29de2fc2d51942f2bb361ed0801421a74947bcb3
koveloper <koveloper@gmail.com>
2022-04-01 11:17:40 +0300

    rebase spinner to use css animation

--- project/src/components/spinner/spinner.tsx
+++ project/src/components/spinner/spinner.tsx
@@ -1,9 +1,21 @@
 function Spinner(): JSX.Element {
   return (
     <div className="spinner">
+      {/* <h2 className="wait-for-connect-title">${this._sign}</h2> */}
       <span>Loading...</span>
       <span>Please wait...</span>
+      <div className="spinner-anime">
+        <div className="spinner-anime-item"></div>
+        <div className="spinner-anime-item"></div>
+        <div className="spinner-anime-item"></div>
+        <div className="spinner-anime-item"></div>
+        <div className="spinner-anime-item"></div>
+      </div>
     </div>
+    // <div className="spinner">
+    //   <span>Loading...</span>
+    //   <span>Please wait...</span>
+    // </div>
   );
 }
 
--- project/src/pages/player-page/player-page.tsx
+++ project/src/pages/player-page/player-page.tsx
@@ -1,9 +1,9 @@
 import Player from '../../components/player/player';
 import Error404 from '../error-404/error-404';
+import Spinner from '../../components/spinner/spinner';
 import { Route, Routes } from 'react-router-dom';
 import { useFilmIdFromUrl } from '../../hooks/use-film-id-from-url';
 import { useFilmData } from '../../hooks/use-film-data';
-import Spinner from '../../components/spinner/spinner';
 
 export default function PlayerPage(): JSX.Element {
   const filmId = useFilmIdFromUrl();
